b0a0f8c00e6bdb1017319b0e5b3cd0162a6f257e
Shaikh Faisal Husain <faisalhs2002@gmail.com>
2023-01-09 22:31:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import MenuGrid from './MenuGrid';
+import React from 'react'
+import MenuCard from './MenuCard';
+import StandardImageList from './ImageList';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <StandardImageList />
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <MenuGrid />
     </div>
-  );
+  )
 }
 
 export default App;
